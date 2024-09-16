83bd06f69f57baee1dd3fbabb0d6f6d3df57251c
scoobysnacku <davidj1234@hotmail.se>
2024-09-16 15:17:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('apexButton').onclick = function() {
+    window.location.href = 'https://www.apexlegends.com';
+};
+
+document.getElementById('valorantButton').onclick = function() {
+    window.location.href = 'https://playvalorant.com';
+};
